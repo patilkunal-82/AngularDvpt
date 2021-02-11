46cf50dd85c52c3217455491583125a6d2c0579d
Kunal Patil <patilkunal@gmail.com>
2021-02-11 10:04:18 -0800

    component2

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,6 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { List } from '../shared/list';
 
+const LISTS: List[] = [
+  {
+    id: '0',
+    name: 'Munich',
+    image: '/assets/images/Munich.png',
+    category: 'photos',
+    featured: true,
+    label: 'New',
+    price: '$ 45.99',
+    description: 'A unique collection of photos of Munich and its surroundings'
+  },
+  {
+    id: '1',
+    name: 'Hypnobirthing',
+    image: '/assets/images/Hypnobirthing.png',
+    category: 'medical',
+    featured: false,
+    label: 'New',
+    price: '$ 15.99',
+    description: 'A natural approach to a safe, easier, more comfortable birthing'
+  },
+  {
+    id: '2',
+    name: 'PractialAR',
+    image: '/assets/images/PracticalAR.png',
+    category: 'technical',
+    featured: false,
+    label: 'New',
+    price: '$ 19.99',
+    description: 'A guide to the technologies, applications and human factors for AR and VR'
+  },
+  {
+    id: '3',
+    name: 'Dabawallas',
+    image: '/assets/images/Dabawallas.png',
+    category: 'management',
+    featured: false,
+    label: '',
+    price: '$ 9.99',
+    description: 'Lessons for building lasting success based on values'
+  }
+
+];
+
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
@@ -8,52 +53,11 @@ import { List } from '../shared/list';
 })
 export class BooksComponent implements OnInit {
 
-  lists: List[] = [
 
-    {
-      id: '0',
-      name: 'Munich',
-      image: '/assets/images/Munich.png',
-      category: 'photos',
-      featured: true,
-      label: 'New',
-      price: '$ 45.99',
-      description: 'A unique collection of photos of Munich and its surroundings'
-    },
-    {
-      id: '1',
-      name: 'Hypnobirthing',
-      image: '/assets/images/Hypnobirthing.png',
-      category: 'medical',
-      featured: false,
-      label: 'New',
-      price: '$ 15.99',
-      description: 'A natural approach to a safe, easier, more comfortable birthing'
-    },
-    {
-      id: '2',
-      name: 'PractialAR',
-      image: '/assets/images/PracticalAR.png',
-      category: 'technical',
-      featured: false,
-      label: 'New',
-      price: '$ 19.99',
-      description: 'A guide to the technologies, applications and human factors for AR and VR'
-    },
-    {
-      id: '3',
-      name: 'Dabawallas',
-      image: '/assets/images/Dabawallas.png',
-      category: 'management',
-      featured: false,
-      label: '',
-      price: '$ 9.99',
-      description: 'Lessons for building lasting success based on values'
-    }
-  ];
+  lists: List[] = LISTS;
+  selectedList: List = LISTS[0];
 
   constructor() { }
-
   ngOnInit() {
   }
 
