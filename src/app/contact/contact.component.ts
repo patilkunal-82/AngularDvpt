01dcea26f41c8f04d7c29236d9a29aed643e45cf
Kunal Patil <patilkunal@gmail.com>
2021-02-17 15:03:48 -0800

    Reactive Forms 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
 
 
+
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -12,11 +14,41 @@ import { Feedback, ContactType } from '../shared/feedback';
 
 export class ContactComponent implements OnInit {
 
-
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
   @ViewChild('fform') feedbackFormDirective;
+
+  formErrors = {
+    'firstname': '',
+    'lastname' : '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required': 'First Name is required.',
+      'minlength' : 'First Name must be at least 2 characters long.',
+      'maxlength' : 'First Name can not be more than 25 characters long.'
+    },
+
+    'lastname': {
+      'required': 'Last Name is required.',
+      'minlength' : 'Last Name must be at least 2 characters long.',
+      'maxlength' : 'Last Name can not be more than 25 characters long.'
+    },
+
+    'telnum': {
+      'required': 'Telephone Nr is required.',
+      'pattern': 'Telephone Nr must contain only numbers.'
+    },
+
+    'email': {
+      'required': 'Email is required.',
+      'email': 'Email not in valid format.'
+    }
+  };
 
   constructor(private fb:FormBuilder) {
    this.createForm();
@@ -42,14 +74,43 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    //valueChanges Observable is provided by Angular framework whenevar 'form' changes
+    this.feedbackForm.valueChanges
+    .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set form validation messages
   }
+
+  onValueChanged(data?: any) {
+
+    if(!this.feedbackForm) {return;}
+
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if(this.formErrors.hasOwnProperty(field)) {
+        //clear previous error messages (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + '';
+            }
+          }
+        }
+      }
+    }
+  }
+
 
 }
