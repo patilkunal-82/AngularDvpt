5bd0e7cfe22aa6f4cb254487e727bb8934cf1c40
Kunal Patil <patilkunal@gmail.com>
2021-02-16 17:32:52 -0800

    Promise Part2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -11,15 +11,27 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    //return Promise.resolve(LEADERS);
+    return new Promise(resolve => {
+      //simulate server latency with 2s delay
+      setTimeout(() => resolve(LEADERS), 2000);
+    });
   }
 
   // java script function filter is used here.
   getLeader (id: string): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+    //return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve => {
+      //simulate server latency with 2s delay
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+  //  return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+  return new Promise(resolve => {
+    //simulate server latency with 2s delay
+    setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
+  });
   }
 }
--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -11,15 +11,27 @@ export class ListService{
 
      // getLists() method is reconfigured to return Promise
       getLists(): Promise<List[]> {
-        return Promise.resolve(LISTS);
+        //return Promise.resolve(LISTS);
+        return new Promise(resolve => {
+          //simulate server latency with 2s delay
+          setTimeout(() => resolve(LISTS), 2000);
+        });
       }
 
     // java script function filter is used here.
       getList(id: string): Promise<List> {
-        return Promise.resolve(LISTS.filter((list) => (list.id === id))[0]);
+      //  return Promise.resolve(LISTS.filter((list) => (list.id === id))[0]);
+      return new Promise(resolve => {
+        //simulate server latency with 2s delay
+        setTimeout(() => resolve(LISTS.filter((list) => (list.id === id))[0]), 2000);
+      });
       }
 
       getFeaturedList(): Promise<List> {
-        return Promise.resolve(LISTS.filter((list) => list.featured)[0]);
+        //return Promise.resolve(LISTS.filter((list) => list.featured)[0]);
+        return new Promise(resolve => {
+          //simulate server latency with 2s delay
+          setTimeout(() => resolve(LISTS.filter((list) => list.featured)[0]), 2000);
+        });
       }
   }
